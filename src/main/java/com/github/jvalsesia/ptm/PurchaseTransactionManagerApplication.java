c55a361b86dd14a1f8895bb011b49b904af7eeac
Julio Valsesia <jvalsesia@gmail.com>
2025-10-02 14:23:09 -0300

    Initial Version

---
+++ src/main/java/com/github/jvalsesia/ptm/PurchaseTransactionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.jvalsesia.ptm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseTransactionManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseTransactionManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jvalsesia/ptm/PurchaseTransactionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jvalsesia.ptm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseTransactionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
